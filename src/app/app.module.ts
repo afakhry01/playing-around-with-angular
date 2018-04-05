84ca50a6e3d04ff19657020c43a43cb9a08152a7
Andrew Fakhry <andrew.fakhry@outlook.com>
2018-04-04 18:30:44 -0600

    Create course form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FavoriteComponent } from './favorite/favorite.component';
 import { ZippyComponent } from './zippy/zippy.component';
+import { FormComponent } from './form/form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FavoriteComponent,
-    ZippyComponent
+    ZippyComponent,
+    FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
